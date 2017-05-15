8c554aac4a4659409cf48663b3b407236beef90e
Oliver Eder <eder@essential-data.sk>
2017-05-15 11:58:27 +0200

    Capitalization preprocessing logic added.

---
+++ src/main/java/com/semanticRelationsExtractor/preprocessing/CapitalizedTokensProcessor.java
@@ -0,0 +1,9 @@
+package com.semanticRelationsExtractor.preprocessing;
+
+/**
+ * Created by Oliver on 5/15/2017.
+ */
+public interface CapitalizedTokensProcessor {
+
+    void process(TrainingDataRow trainingDataRow);
+}
