6a31ef91b68ddba4b6ec4838d418b9617ed811e6
Oliver Eder <eder@essential-data.sk>
2017-05-21 14:59:35 +0200

    Refactored SemanticPreprocessorImpl.

--- src/main/java/com/semanticRelationsExtractor/preprocessing/SemanticPreprocessorImpl.java
+++ src/main/java/com/semanticRelationsExtractor/preprocessing/SemanticPreprocessorImpl.java
@@ -14,11 +14,7 @@ public class SemanticPreprocessorImpl implements SemanticPreprocessor {
 
     @Override
     public SemanticPreprocessingData preprocess(List<String> tokens, List<String> tags) {
-        List<String> filteredTokens = new ArrayList<>();
-        List<String> filteredTags = new ArrayList<>();
         SemanticPreprocessingData semanticPreprocessingData = new SemanticPreprocessingData();
-        int tagsListIndex = 0;
-        int verbIndex = -1;
         int modalVerbIndex = -1;
         boolean containsBeforeVerbPreposition = false;
         int afterVerbFirstPrepositionIndex = -1;
@@ -27,62 +23,109 @@ public class SemanticPreprocessorImpl implements SemanticPreprocessor {
         boolean containsNounAdjectivePredicate = false;
         boolean containsAdverbPredicate = false;
 
-        for (String tag : tags) {
-            if (SemanticExtractionFilterCache.semanticExtractionAllowedTags.contains(tag)) {
-                if (!Tags.DETERMINER.equals(tag) && !Tags.PRONOUN_POSSESIVE.equals(tag)) {
-                    filteredTags.add(tag);
-                    filteredTokens.add(tokens.get(tagsListIndex));
-                    if ((Tags.VERB.equals(tag) || Tags.IS_ARE.equals(tag)) && verbIndex == -1) {
-                        verbIndex = filteredTags.size() - 1;
-                    } else if ((Tags.VERB.equals(tag) || Tags.IS_ARE.equals(tag)) && verbIndex > -1) {
-                        return semanticPreprocessingData;
-                    }
-                    if (Tags.MODAL_VERB.equals(tag)) {
-                        modalVerbIndex = filteredTags.size() - 1;
-                    }
-                    if ((Tags.PREPOSITION.equals(tag) || Tags.TO.equals(tag)) && afterVerbFirstPrepositionIndex == -1 && verbIndex > -1) {
-                        afterVerbFirstPrepositionIndex = filteredTags.size() - 1;
-                    }
-                    if ((Tags.PREPOSITION.equals(tag) || Tags.TO.equals(tag)) && verbIndex == -1) {
-                        containsBeforeVerbPreposition = true;
-                    }
-                    if (Tags.VERB_ING.equals(tag) && verbIndex > -1) {
-                        containsAfterVerbVerbIng = true;
-                    }
-                    if ((Tags.NOUN.equals(tag) || Tags.VERB_ED.equals(tag)) && verbIndex == -1) {
-                        containsSubject = true;
-                    }
-                    if (verbIndex > -1 && tagsListIndex > verbIndex && ((Tags.NOUN.equals(tag) || Tags.ADJECTIVE.equals(tag)) || Tags.VERB_ED.equals(tag) ||
-                            Tags.VERB_ING.equals(tag))) {
-                        containsNounAdjectivePredicate = true;
-                    }
-                    if (verbIndex > -1 && tagsListIndex > verbIndex && Tags.ADVERB.equals(tag)) {
-                        containsAdverbPredicate = true;
-                    }
+        FilteredSentence filteredSentence = filterSentence(tags, tokens);
+        List<String> filteredTags = filteredSentence.getFilteredTags();
+        List<String> filteredTokens = filteredSentence.getFilteredTokens();
 
-                }
-                tagsListIndex++;
-            } else {
-                if (verbIndex > -1) {
-                    break;
-                } else {
-                    return semanticPreprocessingData;
-                }
+        int mainVerbIndex = findMainVerbIndex(filteredTags, Tags.VERB);
+        if (mainVerbIndex == -1) {
+            mainVerbIndex = findMainVerbIndex(filteredTags, Tags.VERB_ED);
+            if (mainVerbIndex == -1) {
+                return semanticPreprocessingData;
             }
         }
-        if (verbIndex == -1 || !containsSubject || (!containsNounAdjectivePredicate && containsAdverbPredicate) ||
-                (Tags.IS_ARE.equals(tags.get(verbIndex)) && !containsNounAdjectivePredicate)) {
+
+        for (int i = 0; i < filteredTags.size(); i++) {
+            String tag = filteredTags.get(i);
+
+            if (Tags.MODAL_VERB.equals(tag)) {
+                modalVerbIndex = i;
+            }
+            if ((Tags.PREPOSITION.equals(tag) || Tags.TO.equals(tag)) && afterVerbFirstPrepositionIndex == -1 && i > mainVerbIndex) {
+                afterVerbFirstPrepositionIndex = i;
+            }
+            if ((Tags.PREPOSITION.equals(tag) || Tags.TO.equals(tag)) && i < mainVerbIndex) {
+                containsBeforeVerbPreposition = true;
+            }
+            if (Tags.VERB_ING.equals(tag) && mainVerbIndex < i) {
+                containsAfterVerbVerbIng = true;
+            }
+            if ((Tags.NOUN.equals(tag) || Tags.VERB_ED.equals(tag)) && i < mainVerbIndex) {
+                containsSubject = true;
+            }
+            if (mainVerbIndex > -1 && i > mainVerbIndex && ((Tags.NOUN.equals(tag) || Tags.ADJECTIVE.equals(tag)) || Tags.VERB_ED.equals(tag) ||
+                    Tags.VERB_ING.equals(tag))) {
+                containsNounAdjectivePredicate = true;
+            }
+            if (mainVerbIndex > -1 && i > mainVerbIndex && Tags.ADVERB.equals(tag)) {
+                containsAdverbPredicate = true;
+            }
+        }
+        if (mainVerbIndex == -1 || !containsSubject || (!containsNounAdjectivePredicate && containsAdverbPredicate) ||
+                (Tags.IS_ARE.equals(tags.get(mainVerbIndex)) && !containsNounAdjectivePredicate)) {
             return semanticPreprocessingData;
         } else {
             semanticPreprocessingData.setTagsList(filteredTags);
             semanticPreprocessingData.setTokensList(filteredTokens);
             semanticPreprocessingData.setContainsBeforeVerbPreposition(containsBeforeVerbPreposition);
-            semanticPreprocessingData.setVerbIndex(verbIndex);
+            semanticPreprocessingData.setVerbIndex(mainVerbIndex);
             semanticPreprocessingData.setModalVerbIndex(modalVerbIndex);
             semanticPreprocessingData.setAfterVerbFirstPrepositionIndex(afterVerbFirstPrepositionIndex);
             semanticPreprocessingData.setContainsAfterVerbVerbIng(containsAfterVerbVerbIng);
             semanticPreprocessingData.setCanGoToExtraction(true);
             return semanticPreprocessingData;
+        }
+    }
+
+    private FilteredSentence filterSentence(List<String> tags, List<String> tokens) {
+        List<String> filteredTokens = new ArrayList<>();
+        List<String> filteredTags = new ArrayList<>();
+
+        for (int i = 0; i < tags.size(); i++) {
+            String tag = tags.get(i);
+            if (SemanticExtractionFilterCache.semanticExtractionAllowedTags.contains(tag)) {
+                if (!Tags.DETERMINER.equals(tag) && !Tags.PRONOUN_POSSESIVE.equals(tag)) {
+                    filteredTags.add(tag);
+                    filteredTokens.add(tokens.get(i));
+                }
+            } else {
+                break;
+            }
+        }
+        return new FilteredSentence(filteredTags, filteredTokens);
+    }
+
+    private int findMainVerbIndex(List<String> tags, String verbTag) {
+        boolean verbFound = false;
+        int verbIndex = -1;
+        for (int i = 0; i < tags.size(); i++) {
+            String tag = tags.get(i);
+            if ((verbTag.equals(tag) || Tags.IS_ARE.equals(tag)) && !verbFound) {
+                verbIndex = i;
+            } else if ((verbTag.equals(tag) || Tags.IS_ARE.equals(tag)) && verbFound) {
+                verbIndex = -1;
+            }
+        }
+        return verbIndex;
+    }
+
+    private class FilteredSentence {
+
+        List<String> filteredTags;
+
+        List<String> filteredTokens;
+
+        public FilteredSentence(List<String> filteredTags, List<String> filteredTokens) {
+            this.filteredTags = filteredTags;
+            this.filteredTokens = filteredTokens;
+        }
+
+        public List<String> getFilteredTokens() {
+            return filteredTokens;
+        }
+
+        public List<String> getFilteredTags() {
+            return filteredTags;
         }
     }
 
